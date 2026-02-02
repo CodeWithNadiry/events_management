f87b0473e3bbb218b4395e178d7fb3a9045d8278
nadiry <usmannadiry75@gmail.com>
2026-02-02 14:20:17 +0500

    changed mongoose

--- backend/app.js
+++ backend/app.js
@@ -52,13 +52,6 @@ app.use((error, req, res, next) => {
 /* ======================
    DATABASE
 ====================== */
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`🚀 Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
