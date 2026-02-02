d168beff96576ae54d8f870318b4b2fc70395e83
nadiry <usmannadiry75@gmail.com>
2026-02-02 16:04:27 +0500

    changed image url

--- frontend/src/components/EventsList.js
+++ frontend/src/components/EventsList.js
@@ -1,26 +1,31 @@
-// import { useLoaderData } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-
 import classes from './EventsList.module.css';
 
-function EventsList({events}) {
-  // const events = useLoaderData();
-
+function EventsList({ events }) {
   return (
     <div className={classes.events}>
       <h1>All Events</h1>
+
       <ul className={classes.list}>
-        {events && events.length > 0 ? events.map((event) => (
-          <li key={event._id} className={classes.item}>
-            <Link to={`/events/${event._id}`}>
-              <img src={`https://events-management-xi.vercel.app${event.image}`} alt={event.title} />
-              <div className={classes.content}>
-                <h2>{event.title}</h2>
-                <time>{event.date}</time>
-              </div>
-            </Link>
-          </li>
-        )): <p>No Events yet.</p>}
+        {events && events.length > 0 ? (
+          events.map((event) => (
+            <li key={event._id} className={classes.item}>
+              <Link to={`/events/${event._id}`}>
+                <img
+                  src={event.image}   // ✅ use Cloudinary URL directly
+                  alt={event.title}
+                />
+
+                <div className={classes.content}>
+                  <h2>{event.title}</h2>
+                  <time>{event.date}</time>
+                </div>
+              </Link>
+            </li>
+          ))
+        ) : (
+          <p>No Events yet.</p>
+        )}
       </ul>
     </div>
   );
