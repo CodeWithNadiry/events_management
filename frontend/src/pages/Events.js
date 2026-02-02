2e61627ab5538378496c6ddbad414c079d13730e
nadiry <usmannadiry75@gmail.com>
2026-02-02 23:02:40 +0500

    added again

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,19 +1,24 @@
+import { useLoaderData } from 'react-router-dom';
+
 import EventsList from '../components/EventsList';
 
-const events = [
-    {
-      "_id": "698083d5ab35002185eafef5",
-      "title": "wedding",
-      "description": "bestjflkjs;dlkf;jalskdfjl;asdkfjsadfj",
-      "image": "https://res.cloudinary.com/djvjx5sxa/image/upload/v1770030037/events/yumqewykbswqxaovi2ca.jpg",
-      "date": "2008-03-01T00:00:00.000Z",
-      "createdAt": "2026-02-02T11:00:37.716Z",
-      "updatedAt": "2026-02-02T11:00:37.716Z",
-      "__v": 0
-    }
-  ]
 function EventsPage() {
+  const events = useLoaderData();
   return <EventsList events={events} />;
 }
 
 export default EventsPage;
+
+
+export async function loader() {
+  const response = await fetch(
+    'https://events-management-xi.vercel.app/events'
+  );
+
+  if (!response.ok) {
+    throw new Error('Could not fetch events.');
+  }
+
+  const data = await response.json();
+  return data.events;
+}
